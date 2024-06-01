3e478a75571bcd993107ba306ad7034e4f09f76b
Lee Lott <llott88@gmail.com>
2024-06-01 05:23:51 -0500

    Initial check in of code

---
+++ Utilities.Email/IEmail.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Mail;
+
+namespace Utilities.Email;
+
+public interface IEmail
+{
+	void SendEmail(string subject, string body, List<string> recipients, List<string> recipientsCc);
+	void SendEmail(string subject, string body, List<string> recipients, List<string> recipientsCc, List<Attachment> attachments);
+	void SendEmail(string subject, string body, string recipients, string recipientsCc, string delimiter);
+	void SendEmail(string subject, string body, string recipients, string recipientsCc, string delimiter, List<Attachment> attachments);
+}
