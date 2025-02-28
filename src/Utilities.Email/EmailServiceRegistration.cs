3ce3001ed8dc50bf984c3e2988ca71b3462687f5
Lee Lott <llott88@gmail.com>
2025-02-27 19:36:48 -0600

    #125 - Added support for MediatR.

---
+++ src/Utilities.Email/EmailServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Utilities.Email;
+
+public static class EmailServiceRegistration
+{
+	public static IServiceCollection AddEmailServices(this IServiceCollection services)
+	{
+		services.AddSingleton<IEmail, Email>();
+		return services;
+	}
+}
---
+++ src/Utilities.Email/MediatR/SendEmailCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Utilities.Email.MediatR;
+
+public class SendEmailCommandHandler(IEmail email) : IRequestHandler<SendEmailCommand>
+{
+	public Task Handle(SendEmailCommand request, CancellationToken cancellationToken)
+	{
+		email.SendEmail(request.Subject, request.Body, request.Recipients, request.RecipientsCc, request.Delimiter);
+		return Task.CompletedTask;
+	}
+}
